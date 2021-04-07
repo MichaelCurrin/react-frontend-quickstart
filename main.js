63da3ac7a9093404fb40ed208a09d5d884de1a1e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-07 22:58:22 +0200

    feat: Add Counter

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ import ReactDOM from "https://dev.jspm.io/react-dom";
 
 const html = htm.bind(React.createElement);
 
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return html`
+    <div>
+      <p>You clicked ${count} times</p>
+
+      <button onClick=${() => setCount(count + 1)}>Click me</button>
+    </div>
+  `;
+}
+
 function App(props) {
   const { name } = props;
 
   return html`
     <div className="App">
       <h2>Hello, ${name}!</h2>
+
+      <${Counter}/>
     </div>
   `;
 }
