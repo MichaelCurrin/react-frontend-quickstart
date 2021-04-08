299fdac3713762d24a45b92ef694dde416d8bd06
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-08 10:52:46 +0200

    build: Lock dependencies

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import htm from "https://dev.jspm.io/htm";
-import React from "https://dev.jspm.io/react";
-import ReactDOM from "https://dev.jspm.io/react-dom";
+import htm from "https://dev.jspm.io/htm@3.0";
+import ReactDOM from "https://dev.jspm.io/react-dom@17.0";
+import React from "https://dev.jspm.io/react@17.0";
 
 const html = htm.bind(React.createElement);
 
